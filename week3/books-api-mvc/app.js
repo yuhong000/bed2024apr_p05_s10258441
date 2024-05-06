02c95a2e953bd382abef39642ea38627670baa46
yuhong000 <s10258441@connect.np.edu.sg>
2024-05-06 13:31:18 +0800

    commit

---
+++ week3/books-api-mvc/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksController = require("./controllers/booksController"); // Import controllers
+const validateBook = require("./middlewares/validateBook");
+
+const app = express();
+
+app.use(bodyParser.json()); // Parse incoming JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+// Define individual routes for each controller function
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
+app.delete("/books/:id", booksController.deleteBook);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
