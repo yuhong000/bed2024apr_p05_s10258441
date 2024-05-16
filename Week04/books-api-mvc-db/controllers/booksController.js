9b04c2516ff9051b6ee4f7ea22a705b13cbaaf76
yuhong000 <s10258441@connect.np.edu.sg>
2024-05-16 17:51:27 +0800

    commit

---
+++ Week04/books-api-mvc-db/controllers/booksController.js
@@ -0,0 +1,30 @@
+const Book = require("../models/book");
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.getAllBooks();
+    res.json(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving books");
+  }
+};
+
+const getBookById = async (req, res) => {
+  const bookId = parseInt(req.params.id);
+  try {
+    const book = await Book.getBookById(bookId);
+    if (!book) {
+      return res.status(404).send("Book not found");
+    }
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving book");
+  }
+};
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+};
---
+++ Week04/books-api-mvc-db/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  user: "bookapi-user", // Replace with your SQL Server login username
+  password: "123", // Replace with your SQL Server login password
+  server: "localhost",
+  database: "bed_db",
+  trustServerCertificate: true,
+  options: {
+    port: 1433, // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
